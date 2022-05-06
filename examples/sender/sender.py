6e3e32d1fb390432645a86aa1936439f8a24b260
Ryan C James <rcjames1004@gmail.com>
2022-05-06 10:02:50 -0700

    Update sender.py

--- examples/sender/sender.py
+++ examples/sender/sender.py
@@ -2,9 +2,9 @@
     sender.py
     ~~~~~~~~~~~~~~
 
-    Sends dummy data
+    This example demonstrates how to integrate a surgical device with dopl connect.
 
-    :copyright: 2020 Ryan James
+    :copyright: 2022 Ryan James
 """
 from os import path
 
